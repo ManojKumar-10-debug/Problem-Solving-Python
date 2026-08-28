71c40f4a7fec4a09f059450c16ceb0351517b15f
ManojKumar <codingwithmanoj10@gmail.com>
2026-08-28 13:00:43 +0530

    Added comments

--- DataTypes/Add.py
+++ DataTypes/Add.py
@@ -1,3 +1,15 @@
+        # First take two numbers
+        #        ||
+        # Take input
+        #        ||
+        # Add the two numbers
+        #        ||
+        # Store the result in c
+        #        ||
+        # Print c
+
+
+
 first_number = int(input())
 second_number = int(input())
 
