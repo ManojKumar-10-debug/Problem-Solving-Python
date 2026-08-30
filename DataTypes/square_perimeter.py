d6f45658a9768a15f1d4de097658fadd311f5bd8
ManojKumar <codingwithmanoj10@gmail.com>
2026-08-30 21:17:00 +0530

    Added rectangle_perimeter.py

---
+++ DataTypes/square_perimeter.py
@@ -0,0 +1,15 @@
+# Notes:
+# 1. The problem asks us to find the perimeter of a square using its side.
+# 2. side stores the integer input given by the user.
+# 3. square_perimeter stores the calculated perimeter.
+# 4. I calculate the perimeter using 4 * side.
+# 5. The calculated square_perimeter is printed as the output.
+# 6. The main concept used is arithmetic calculation with variables.
+
+import DataTypes
+
+side = int(input())
+
+square_perimeter = 4 * side    # formula
+
+print(square_perimeter)
